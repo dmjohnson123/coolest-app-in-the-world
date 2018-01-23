3fcd5762144c042afc4efea460af6fafff70abd0
dmjohnson <dmjohnson@wlapcs.org>
2018-01-23 19:06:21 +0000

    first commit

--- sketch.js
+++ sketch.js
@@ -1,6 +1,17 @@
+var rickyaSlider, godellSlider, billSlider;
+
 function setup(){
     createCanvas(600,400);
     background(0);
+    
+    rickyaSlider = createSlider(0, 255, 255);
+    rickyaSlider.position(20,20);
+    
+     godellSlider = createSlider(0, 255, 255);
+    godellSlider.position(20,50);
+    
+     billSlider = createSlider(0, 255, 255);
+    billSlider.position(20,80);
 }
 
 function draw(){
